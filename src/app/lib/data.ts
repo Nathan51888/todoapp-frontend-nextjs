62a57e0f63f05a32060ef0c37235e870e7d71e5c
Nathan51888 <NathanLiang99@outlook.com>
2025-03-19 17:46:39 +1300

    Added auth for todo fetching

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -15,12 +15,19 @@ export async function getUserProfile(): Promise<UserProfile> {
 }
 
 export async function getAllTodos(): Promise<TodoList> {
-    const res = await fetch("http://localhost:8080/todo")
+    const cookieStore = await cookies()
+    const token = cookieStore.get("refreshToken")
+    const res = await fetch("http://localhost:8080/todo", {
+        method: "GET",
+        headers: { "Authorization": token?.value as string }
+    })
     const data = await res.json()
     return data
 }
 
 export async function createTodo(title: string): Promise<TodoObject> {
+    const cookieStore = await cookies()
+    const token = cookieStore.get("refreshToken")
     const res = await fetch("http://localhost:8080/todo", {
         method: "POST",
         body: JSON.stringify({
@@ -28,7 +35,8 @@ export async function createTodo(title: string): Promise<TodoObject> {
             completed: false,
         }),
         headers: {
-            "Content-type": "application/json"
+            "Content-type": "application/json",
+            "Authorization": token?.value as string,
         }
     })
     const data = await res.json()
@@ -37,8 +45,11 @@ export async function createTodo(title: string): Promise<TodoObject> {
 }
 
 export async function updateTodoCompleted(id: string, completed: boolean): Promise<TodoObject> {
+    const cookieStore = await cookies()
+    const token = cookieStore.get("refreshToken")
     const res = await fetch(`http://localhost:8080/todo?id=${id}&completed=${completed}`, {
-        method: "PUT"
+        method: "PUT",
+        headers: { "Authorization": token?.value as string }
     })
     const data = await res.json()
     console.log(data)
@@ -46,8 +57,11 @@ export async function updateTodoCompleted(id: string, completed: boolean): Promi
 }
 
 export async function updateTodoTitle(id: string, title: string) {
+    const cookieStore = await cookies()
+    const token = cookieStore.get("refreshToken")
     const res = await fetch(`http://localhost:8080/todo?id=${id}&title=${title}`, {
-        method: "PUT"
+        method: "PUT",
+        headers: { "Authorization": token?.value as string }
     })
     const data = await res.json()
     console.log("updateTodoTitle fetched data: ", data)
@@ -55,8 +69,11 @@ export async function updateTodoTitle(id: string, title: string) {
 }
 
 export async function deleteTodoById(id: string): Promise<TodoObject> {
+    const cookieStore = await cookies()
+    const token = cookieStore.get("refreshToken")
     const res = await fetch("http://localhost:8080/todo?id=" + id, {
         method: "DELETE",
+        headers: { "Authorization": token?.value as string }
     })
     const data = await res.json()
     console.log(data)
