940352b39f85cf51b497287deea12fb2626654e4
Nathan51888 <NathanLiang99@outlook.com>
2025-03-23 14:43:51 +1300

    Removed styling on nav links

--- src/app/components/nav/top-nav.tsx
+++ src/app/components/nav/top-nav.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 export default function TopNav() {
     return (
         <nav className="flex justify-start items-center gap-3 w-full p-6 bg-slate-500">
-            <Link href={"/"} className="bg-white">Home</Link>
-            <Link href={"/dashboard"} className="bg-white">Dashboard</Link>
-            <Link href={"/login"} className="bg-white">Login</Link>
+            <Link href={"/"}>Home</Link>
+            <Link href={"/dashboard"}>Dashboard</Link>
+            <Link href={"/login"}>Login</Link>
+            <Link href={"/register"}>Register</Link>
             <Link href={"/profile"}>Profile</Link>
         </nav>
     )
